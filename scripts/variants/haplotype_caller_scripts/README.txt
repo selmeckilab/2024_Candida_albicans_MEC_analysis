305ee4dac4c33708482f38e160c06b679106ba6e
Nancy Scott <scot0854@umn.edu>
2025-03-18 09:55:20 -0500

    new script for annotating and filtering GATK VCF

---
+++ scripts/variants/haplotype_caller_scripts/README.txt
@@ -0,0 +1,28 @@
+# Variant calling with haplotype caller
+
+One of the benefits of using GATK haplotype caller is that it allows joint
+genotyping of samples added at different times, through the use of a database.
+Joint genotyping for a population of samples is best practice - it's not good
+to separately call variants on samples individually and then combine them.
+
+## Workflow:
+
+1. Variants are called on individual samples. See "haplotypecaller_1.sh" which is
+an array with 1 job per sample
+
+2. A sample map is generated, see "samplemap.sh". You can also just manually make
+this, it's a tabbed file with sample name and path to individual vcf
+
+3. Samples are added to the database. To create a new database, see
+"genomicsdb_new.sh", and to add samples to an existing database, see
+"genomicsdb_update.sh". These scripts use the "intervals.list" file which in our
+case is just the chromosome names and full length of chromosomes from 1-NNN.
+
+4. Joint genotyping of all samples is performed. See
+"joint_genotyping_filtering.sh", which uses hard filtering parameters as
+recommended by GATK for non-model organisms.
+
+All of the scripts can be run sequentially, see "depend_haplotype.sh".
+
+For further filtering, annotation with gene names (if available for your
+species) and further filtering using bcftools, see "annotate_filter.sh".
---
+++ scripts/variants/haplotype_caller_scripts/annotate_filter.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --mem=10gb
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=scot0854@umn.edu
+#SBATCH --time=1:00:00
+#SBATCH -p msilarge,msismall
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+
+set -ue
+set -o pipefail
+
+export BCFTOOLS_PLUGINS=/home/selmecki/shared/software/software.install/bcftools/1.17/plugins
+
+regions=SC5314_A21_regions.bed
+species=Calbicans
+ref=SC5314-A21
+gene_file=Calbicans_SC5314_A21_sorted_genes.bed.gz
+gene_vcf=Calbicans_genes.vcf.gz
+fasta=/home/selmecki/shared/disaster_recovery/Reference_Genomes/SC5314_A21/C_albicans_SC5314_version_A21-s02-m09-r08_chromosomes.fasta
+bcftools_out=Calbicans_filtered.vcf.gz  # include .vcf
+snpeff=/home/selmecki/shared/software/snpEff/snpEff.jar
+snpeff_config=/home/selmecki/shared/software/snpEff/snpEff.config
+snpeff_db=SC5314_s02m09r08  # must be in snpeff.config file and must be name of directory in snpeff data subdir
+annotate_vcf=Calbicans_bwa_filtered_annotated.vcf # include .vcf
+genotype_table=Calbicans_bwa_genotypes.txt  # tab delimited table for use with R scripts (MCA using FactoMineR)
+
+# Load modules
+module use /home/selmecki/shared/software/modulefiles.local
+
+module load bcftools/1.17
+module load htslib
+
+# Remove intermediate files.
+function finish {
+  rm "${gene_vcf}"*
+  rm "${species}_${ref}"_regions.vcf*
+  rm "${bcftools_out}"
+ }
+
+ trap finish EXIT
+
+# Filter out repetitive regions and compress
+bcftools view \
+    -R "${regions}" \
+    "${species}"_"${ref}"_merged.vcf.gz \
+    -Oz \
+    -o "${species}"_"${ref}"_regions.vcf.gz
+
+bcftools index "${species}"_"${ref}"_regions.vcf.gz
+
+# Add gene name annotation as info column.
+bcftools annotate \
+    -a "${gene_file}" \
+    -c CHROM,FROM,TO,GENE \
+    -h <(echo '##INFO=<ID=GENE,Number=1,Type=String,Description="Gene name">') \
+    -o "${gene_vcf}" \
+    "${species}"_"${ref}"_regions.vcf.gz
+
+bcftools index "${gene_vcf}"
+
+# Quality and frequency hard filtering.
+bcftools norm -f "${fasta}" -m -indels \
+    "${gene_vcf}" \
+    | bcftools +fill-tags - -- -t "FORMAT/VAF" \
+    | bcftools view -i "(GT='het' & FORMAT/VAF > 0.14 & FORMAT/VAF < 0.85) | (GT='AA' & FORMAT/VAF > 0.98)" \
+    | bcftools view -e 'GT[0]="alt"' -Ou \
+    | bcftools view -c 1 \
+    -Oz -o "${bcftools_out}"
+
+# Annotate using snpeff with manually built database.
+java -Xmx9g -jar "${snpeff}" -c "${snpeff_config}" "${snpeff_db}" \
+"${bcftools_out}" >  "${annotate_vcf}"
+
+# Subset to biallelic SNPs, output tab-delimited genotype file,
+# use for multiple correspondence analysis
+module unload bcftools
+module load bcftools/1.9
+
+bcftools view -m2 -M2 -v "${annotate_vcf}" \
+    | bcftools view -e 'GT="mis"' \
+    | bcftools query -H -f '%CHROM\t%POS[\t%GT]\n' >> "${genotype_table}"
+
+sed -i '1s/\[[0-9]\+\]//g' "${genotype_table}"
+sed -i '1s/\:GT//g' "${genotype_table}"
+sed -i '1s/^\# //' "${genotype_table}"
+
+# Zip and index output.
+bgzip "${annotate_vcf}"
+tabix "${annotate_vcf}".gz
